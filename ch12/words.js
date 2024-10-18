a940d070d96bcad5a40596e748bb6eb726a76da4
TestAquatic <testaquatic@notaemail.com>
2024-10-18 20:55:07 +0900

    words

---
+++ ch12/words.js
@@ -0,0 +1,27 @@
+function words(s) {
+    var r = /\s+|$/g;
+    r.lastIndex = s.match(/[^ ]/).index;
+
+    return {
+        [Symbol.iterator]() {
+            return this;
+        },
+
+        next() {
+            let start = r.lastIndex;
+            if (start < s.length) {
+                let match = r.exec(s);
+                if (match) {
+                    return {
+                        value: s.substring(start, match.index)
+                    };
+                }
+            }
+            return {
+                done: true
+            };
+        }
+    };
+}
+
+[...words("   abc def ghi! ")];
